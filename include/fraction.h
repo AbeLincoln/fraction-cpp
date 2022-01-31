226cc8a06b9db0f02802f20ef18c96fa2eb22ddc
unknown <safekyle@gmail.com>
2022-01-30 20:52:36 -0500

    Added casting methods and associated tests, comparisons and associated tests, cleaned up arithmetic tests

--- include/fraction.h
+++ include/fraction.h
@@ -47,21 +47,17 @@ public:
   // TODO Implement and extend the other assignment operators, if time allows.
 
   // Comparators
-  // TODO Extend '==' operator if time allows
-  bool equals(Fraction);
-  // TODO Extend '!=' operator if time allows
-  bool notEquals(Fraction);
-  // TODO Extend '>' operator if time allows
-  bool isGreatThan(Fraction);
-  // TODO Extend '<' operator if time allows
-  bool isLessThan(Fraction);
-  // TODO Should implement and extend '<=' operator if other comparators are extended because a user would expect it
-  // TODO Should implement and extend '>=' operator if other comparators are extended because a user would expect it
+  bool operator == (const Fraction);
+  bool operator != (const Fraction);
+  bool operator > (const Fraction);
+  bool operator < (const Fraction);
+  bool operator <= (const Fraction);
+  bool operator >= (const Fraction);
 
   // Casting
   friend std::ostream& operator << (std::ostream&, const Fraction&);
-  // TODO Extend 'float()' if time allows
-  // TODO Extend 'bool()' at the end, if time allows
+  operator float();
+  operator bool();
 
 };
 
